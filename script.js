84cd57183872ad2357fe019b6a8b8a0eed0d5432
Anshuman Singh <anshumansingh005@gmail.com>
2023-07-11 16:48:37 +0530

    script.js updated

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 "use strict";
-console.log("Hello World");
-console.log("new data");
-console.log("nothign");
+const rows = 100;
+const cols = 26;
+const addressColCont = document.querySelector(".address-col-cont");
+for (let i = 0; i < rows; i++) {
+    const addressCol = document.querySelector("div");
+    addressCol === null || addressCol === void 0 ? void 0 : addressCol.innerText = toString(i + 1);
+}
--- script.ts
+++ script.ts
@@ -1,3 +1,11 @@
-console.log("Hello World");
-console.log("new data");
-console.log("nothign");
+const rows: number = 100;
+const cols: number = 26;
+
+const addressColCont = document.querySelector(
+  ".address-col-cont"
+) as HTMLDivElement | null;
+for (let i: number = 0; i < rows; i++) {
+  const addressCol = document.querySelector("div") as HTMLDivElement | null;
+
+  addressCol?.innerText = toString(i + 1);
+}
