173b1ed0d71e5f359ff9265ae2c02d79ee5653af
Anshuman Singh <anshumansingh005@gmail.com>
2023-07-21 17:15:51 +0530

    address row and address col is added

--- script.js
+++ script.js
@@ -2,7 +2,17 @@
 const rows = 100;
 const cols = 26;
 const addressColCont = document.querySelector(".address-col-cont");
+
+const addressRowCont = document.querySelector(".address-row-cont");
 for (let i = 0; i < rows; i++) {
-    const addressCol = document.querySelector("div");
-    addressCol === null || addressCol === void 0 ? void 0 : addressCol.innerText = toString(i + 1);
+  const addressCol = document.createElement("div");
+  addressCol.setAttribute("class", "address-col");
+  addressCol.innerText = (i + 1).toString();
+  addressColCont.appendChild(addressCol);
+}
+for (let i = 0; i < cols; i++) {
+  const addressRow = document.createElement("div");
+  addressRow.setAttribute("class", "address-row");
+  addressRow.innerText = (i + 1).toString();
+  addressRowCont.appendChild(addressRow);
 }
--- script.ts
+++ script.ts
@@ -1,11 +1,21 @@
 const rows: number = 100;
 const cols: number = 26;
 
-const addressColCont = document.querySelector(
-  ".address-col-cont"
-) as HTMLDivElement | null;
-for (let i: number = 0; i < rows; i++) {
-  const addressCol = document.querySelector("div") as HTMLDivElement | null;
+const addressColCont: HTMLElement | null =
+  document.querySelector(".address-col-cont");
 
-  addressCol?.innerText = toString(i + 1);
+for (let i: number = 0; i < rows; i++) {
+  const addressCol: HTMLDivElement = document.createElement("div");
+
+  addressCol.setAttribute("class", "address-col");
+
+  addressCol.innerText = (i + 1).toString();
+  addressColCont!.appendChild(addressCol);
+}
+for (let i: number = 0; i < cols; i++) {
+  const addressRow: HTMLDivElement = document.createElement("div");
+  addressRow.setAttribute("class", "address-row");
+  addressRow.innerText = (i + 1).toString();
+
+  addressRow!.appendChild(addressRow);
 }
