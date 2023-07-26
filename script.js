9433133573a5f035a9148a733bf5cb478a264d9c
Anshuman Singh <anshumansingh005@gmail.com>
2023-07-26 16:34:46 +0530

    added remaining grid cells like Excel.

--- script.js
+++ script.js
@@ -2,17 +2,37 @@
 const rows = 100;
 const cols = 26;
 const addressColCont = document.querySelector(".address-col-cont");
-
 const addressRowCont = document.querySelector(".address-row-cont");
+const cellsCont = document.querySelector(".cells-cont");
+const addressBar = document.querySelector(".address-bar");
+function addListenerOnAddressBarDispaly(cell, i, j) {
+    cell === null || cell === void 0 ? void 0 : cell.addEventListener("click", (e) => {
+        const rowId = i + 1;
+        const colId = String.fromCharCode(65 + j);
+        addressBar.value = addressBar.innerText = `${colId}${rowId}`;
+    });
+}
 for (let i = 0; i < rows; i++) {
-  const addressCol = document.createElement("div");
-  addressCol.setAttribute("class", "address-col");
-  addressCol.innerText = (i + 1).toString();
-  addressColCont.appendChild(addressCol);
+    const addressCol = document.createElement("div");
+    addressCol.setAttribute("class", "address-col");
+    addressCol.innerText = (i + 1).toString();
+    addressColCont.appendChild(addressCol);
 }
 for (let i = 0; i < cols; i++) {
-  const addressRow = document.createElement("div");
-  addressRow.setAttribute("class", "address-row");
-  addressRow.innerText = (i + 1).toString();
-  addressRowCont.appendChild(addressRow);
+    const addressRow = document.createElement("div");
+    addressRow.setAttribute("class", "address-row");
+    addressRow.innerText = String.fromCharCode(65 + i);
+    addressRowCont.appendChild(addressRow);
+}
+for (let i = 0; i < rows; i++) {
+    const rowCont = document.createElement("div");
+    rowCont.setAttribute("class", "row-cont");
+    for (let j = 0; j < cols; j++) {
+        let cell = document.createElement("div");
+        cell.setAttribute("class", "cell");
+        cell.setAttribute("contenteditable", "true");
+        rowCont.appendChild(cell);
+        addListenerOnAddressBarDispaly(cell, i, j);
+    }
+    cellsCont === null || cellsCont === void 0 ? void 0 : cellsCont.appendChild(rowCont);
 }
--- script.ts
+++ script.ts
@@ -1,12 +1,31 @@
 const rows: number = 100;
 const cols: number = 26;
 
-const addressColCont: HTMLElement | null =
+const addressColCont: HTMLDivElement | null =
   document.querySelector(".address-col-cont");
+
+const addressRowCont: HTMLDivElement | null =
+  document.querySelector(".address-row-cont");
+
+const cellsCont: HTMLDivElement | null = document.querySelector(".cells-cont");
+
+const addressBar: HTMLInputElement | null =
+  document.querySelector(".address-bar");
+
+function addListenerOnAddressBarDispaly(
+  cell: HTMLElement,
+  i: number,
+  j: number
+): void {
+  cell?.addEventListener("click", (e: MouseEvent): void => {
+    const rowId: number = i + 1;
+    const colId: String = String.fromCharCode(65 + j);
+    addressBar!.value = addressBar!.innerText = `${colId}${rowId}`;
+  });
+}
 
 for (let i: number = 0; i < rows; i++) {
   const addressCol: HTMLDivElement = document.createElement("div");
-
   addressCol.setAttribute("class", "address-col");
 
   addressCol.innerText = (i + 1).toString();
@@ -15,7 +34,21 @@ for (let i: number = 0; i < rows; i++) {
 for (let i: number = 0; i < cols; i++) {
   const addressRow: HTMLDivElement = document.createElement("div");
   addressRow.setAttribute("class", "address-row");
-  addressRow.innerText = (i + 1).toString();
+  addressRow.innerText = String.fromCharCode(65 + i);
 
-  addressRow!.appendChild(addressRow);
+  addressRowCont!.appendChild(addressRow);
+}
+
+for (let i: number = 0; i < rows; i++) {
+  const rowCont: HTMLDivElement | null = document.createElement("div");
+  rowCont.setAttribute("class", "row-cont");
+
+  for (let j: number = 0; j < cols; j++) {
+    let cell: HTMLDivElement | null = document.createElement("div");
+    cell.setAttribute("class", "cell");
+    cell.setAttribute("contenteditable", "true");
+    rowCont.appendChild(cell);
+    addListenerOnAddressBarDispaly(cell, i, j);
+  }
+  cellsCont?.appendChild(rowCont);
 }
